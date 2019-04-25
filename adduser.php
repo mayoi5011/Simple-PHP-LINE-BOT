cf2167194227b947e66401a9ac41ef5bd9626d3b
mayoi5011 <46701036+mayoi5011@users.noreply.github.com>
2019-04-25 23:57:21 +0700

    Add files via upload

---
+++ adduser.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "Adicet@212";
+$dbname = "project_erdi";
+
+/////////////////////////////Create connection/////////////////////////////////
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+mysqli_set_charset($conn,"utf8");
+
+$sql = "SELECT * FROM `user`";
+$result = $conn->query($sql);
+$data = array();
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row['id'];
+    }
+} else {
+   echo "no data"
+}
+echo json_encode($data);
+$conn->close();
+?> 
