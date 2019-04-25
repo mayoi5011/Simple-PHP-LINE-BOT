dd9a67bc088c59fb6adb92ac2b0178601733ae7c
mayoi5011 <46701036+mayoi5011@users.noreply.github.com>
2019-04-26 00:43:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 require_once __DIR__ . '/lineBot.php';
+$message = $_GET['msg'];
 
 $bot = new Linebot();
 $text = $bot->getMessageText();
-$bot->reply($text);
+$bot->reply($message);
 
 ?>
 
