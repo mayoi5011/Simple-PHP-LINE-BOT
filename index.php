6a8d6f9b73d510f4ffb327b4e5d8b9429bbfc275
mayoi5011 <46701036+mayoi5011@users.noreply.github.com>
2019-04-26 00:40:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,10 @@
 <?php
-/*require_once __DIR__ . '/lineBot.php';
+require_once __DIR__ . '/lineBot.php';
 
 $bot = new Linebot();
 $text = $bot->getMessageText();
 $bot->reply($text);*/
 
-$url = "https://apecpv.cmru.ac.th/projecterdi/adduser.php?id=Ub5bd2d0b18e3e8f76cd94e897f05c654";
-$json = file_get_contents($url);
-$obj = json_decode($json);
-var_dump($obj);
-			/*foreach($obj as $key=>$value)
-			{
-				$resule = $value;
-			}
-		    
-			$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-			$arrayPostData['messages'][0]['type'] = "text";
-			$arrayPostData['messages'][0]['text'] = $resule;
-			
-			*/
-			/*if($resule == "no"){
-				$arrayPostData['messages'][0]['text'] = "ฉันจำได้ว่าคุณสมัครสมาชิกไปแล้วนะ";
-			}else if($resule == "ok"){
-				$arrayPostData['messages'][0]['text'] = "สมัครสมาชิกแล้วจร้าาาาาา";
-			}else if($resule == "error"){
-				$arrayPostData['messages'][0]['text'] = "มีบางอย่างผิดพลาด";
-			}*/
-		    
 ?>
 
 
