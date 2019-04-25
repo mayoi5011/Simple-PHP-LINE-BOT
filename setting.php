ef9c479633b8624b0bdccf019f0103ca3d4d05ab
mayoi5011 <46701036+mayoi5011@users.noreply.github.com>
2019-04-25 23:43:14 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,11 +2,11 @@
 
 class Setting {
 	public static function getChannelAccessToken(){
-		$channelAccessToken = "";
+		$channelAccessToken = "oKfXSEXkmDRNdxcUrkAJrvoJ49gdmeCM+MoMbpLXOKXicGis8P7YsIWT0f5BBYP9h3xjTVxRkUdyQGKr3rg6X5nRmSnIMEe7w9+oZ4fKO8d6rEZeiGruIfNwV5eFmsMoPHbb9fdbw92/nWN/jsPpPgdB04t89/1O/w1cDnyilFU=";
 		return $channelAccessToken;
 	}
 	public static function getChannelSecret(){
-		$channelSecret = "";
+		$channelSecret = "e03fcec4b046f94fb30f5cf71b262648";
 		return $channelSecret;
 	}
 	public static function getApiReply(){
