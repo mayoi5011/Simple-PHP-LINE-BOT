31777760f9bbab3f33f3dec421429d5a7afe2e79
mayoi5011 <46701036+mayoi5011@users.noreply.github.com>
2019-04-26 00:17:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,27 @@ $bot = new Linebot();
 $text = $bot->getMessageText();
 $bot->reply($text);
 
+$url = "https://apecpv.cmru.ac.th/projecterdi/adduser.php?id=".$id;
+$json = file_get_contents($url);
+$obj = json_decode($json);
+			foreach($obj as $key=>$value)
+			{
+				$resule = $value;
+			}
+		    
+			$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+			$arrayPostData['messages'][0]['type'] = "text";
+			$arrayPostData['messages'][0]['text'] = $resule;
+			/*if($resule == "no"){
+				$arrayPostData['messages'][0]['text'] = "ฉันจำได้ว่าคุณสมัครสมาชิกไปแล้วนะ";
+			}else if($resule == "ok"){
+				$arrayPostData['messages'][0]['text'] = "สมัครสมาชิกแล้วจร้าาาาาา";
+			}else if($resule == "error"){
+				$arrayPostData['messages'][0]['text'] = "มีบางอย่างผิดพลาด";
+			}*/
+		    
+$bot->reply($arrayHeader,$arrayPostData);
+?>
+
 
 
